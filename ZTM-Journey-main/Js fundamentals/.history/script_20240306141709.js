ba035b797e9f6f52877f99d6d79f09f150c804be
lah23500 <104540971+lah23500@users.noreply.github.com>
2024-03-06 14:53:15 -0600

    commit

---
+++ ZTM-Journey-main/Js fundamentals/.history/script_20240306141709.js
@@ -0,0 +1,62 @@
+// 5 + "34" // "534"
+// 5 - "4" // 1
+// 10 % 5 // 0
+// 5 % 10 // 5
+// "Java" + "Script" // "JavaScript"
+// " " + " " //  "  "
+// " " + 0 //  " 0"
+// true + true // 2
+// true + false // 1
+// false + true //  1
+// false - true //  -1
+// 3 - 4 //  -1
+// "Bob" - "bill" // NAN
+
+// 5 >= 1 // true
+// 0 === 1 //  false
+// 4 <= 1 //  false
+// 1 != 1 //  false
+// "A" > "B" //  false
+// "B" < "C" //  true
+// "a" > "A" //  true
+// "b" < "A" //  false
+// true === false //  false
+// true != true // false
+
+// // Make the string: "HI There! It's sunny out" by using the + sign.
+// "Hi There! " + "It\'s \"sunny\" out"
+
+// Number: numbers
+
+// Striing "" ''
+
+// Boolean <> ==
+
+// Variables must start with a letter, and can end with a number and never a number & symbol
+// variables can hold booleans, strings & numbers.
+
+// undefined JS type means that the variable you declared has not been assigned
+
+// This is a conditional statement:
+
+var name = Billy;
+
+if (name === "Billy") {
+  alert("hi Billy");
+}
+
+if (name === "Billy") {
+  alert("Hi Billy");
+} else {
+  alert("I do not know you");
+}
+
+// else if statements:
+
+if (name === "Billy") {
+  alert("Hi Billy!");
+} else if (name === "Susy") {
+  alert("Hi Susy!");
+} else {
+  alert("I do not know you!");
+}
